ac2e60d4088036a8d9f5b644bcc0d076002ee1c0
huangxl <413612464@qq.com>
2025-12-02 10:10:16 +0800

    feat: 你好C++

---
+++ demo/01helloCpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "你好 c" << endl;
+  cout << "你好 c+" << endl;
+  cout << "你好 c++" << endl;
+
+  // system("pause");
+  return 0;
+}
