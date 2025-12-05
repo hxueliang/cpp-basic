307ab2fec66eeeff2a19fdfe106ed5d62936c0be
huangxl <413612464@qq.com>
2025-12-05 11:30:19 +0800

    feat: fstream多行读取

--- demo/12fstream多行读取.cpp
+++ demo/12fstream多行读取.cpp
@@ -14,6 +14,35 @@ int main()
     return 0;
   }
 
+  char str[3][1024] = {0};
+
+  /**
+   * 方法一
+   */
+  // file.get(str[0], 1024);
+  // file.get(); // 读取换行符
+  // file.get(str[1], 1024);
+  // file.get(); // 读取换行符
+  // file.get(str[2], 1024);
+  //
+  // for (int i = 0; i < 3; i++)
+  // {
+  //   cout << str[i] << endl;
+  // }
+
+  /**
+   * 方法二
+   */
+  for (int i = 0; i < 3; i++)
+  {
+    file.getline(str[i], 1024);
+  }
+
+  for (int i = 0; i < 3; i++)
+  {
+    cout << str[i] << endl;
+  }
+
   file.close();
 
   return 0;
